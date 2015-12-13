e6380014b9f2a5097d4b9b5370f2631ec500dd9c
Fabrizio Moscon <mosconfabrizio@gmail.com>
2015-12-13 21:17:05 +0000

    add example of REST API uses
    - list roles
    - list channels
    - list general channel members
    - list general channel messages

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+var util = require('util')
 var credentials = require('./credentials.json');
 var express = require('express');
 var TokenProvider = require('./lib/tokenprovider');
@@ -20,3 +21,48 @@ app.get('/getToken', function(req, res) {
 app.use(express.static(__dirname + '/public'));
 
 app.listen(8080);
+
+
+var IpMessagingClient = require('twilio').IpMessagingClient;
+
+var client = new IpMessagingClient(credentials.accountSid, credentials.authToken);
+var service = client.services(credentials.instanceSid);
+
+// roles
+service.roles.list().then(function(response) {
+  console.log('ROLES', util.inspect(response, false, 5, true));
+}).fail(function(error) {
+  console.log(error);
+});
+
+var GENERAL_CHANNEL_NAME = 'General Chat Channel'
+
+// channels
+service.channels.list().then(function(response) {
+  console.log('CHANNELS', util.inspect(response, false, 5, true));
+
+  var _findGeneral = function(channel) {
+    return channel.friendly_name == GENERAL_CHANNEL_NAME
+  }
+
+  var general = response.channels.filter(_findGeneral)[0]
+
+  var generalChannelSid = general.sid
+
+  // messages
+  service.channels(generalChannelSid).messages.list().then(function(response) {
+    console.log('MESSAGES in GENERAL', util.inspect(response, false, 5, true));
+  }).fail(function(error) {
+    console.log(error);
+  });
+
+  // members
+  service.channels(generalChannelSid).members.list().then(function(response) {
+    console.log('MEMBERS in GENERAL', util.inspect(response, false, 5, true));
+  }).fail(function(error) {
+    console.log(error);
+  });
+
+}).fail(function(error) {
+  console.log(error);
+});
